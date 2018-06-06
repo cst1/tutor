aa320cdd08dedf4661800a6bc96efbcca4eafa9d
Simon Hunt <sdhunt@gmail.com>
2018-06-06 11:56:32 -0700

    Converted entries to javascript object structure.

---
+++ docs/computerphile/episodes.js
@@ -0,0 +1,50 @@
+// Data for Computerphile episodes
+
+computerphile = [
+  {
+    v: 'DW4f1x6IQ9o',
+    t: 'Welcome to Computerphile!',
+    d: '15th April 2013',
+    n: `Introduction to the channel.`
+  },
+
+  {
+    v: 'pcR7ylW-Gok',
+    t: 'Homemade Video Arcade Machine',
+    d: '17th April 2013',
+    n: `Defender etc.`
+  },
+
+  {
+    v: 'LHSSY8QNvew',
+    t: 'Follow the Cookie Trail',
+    d: '21st May 2013',
+    n: `Discussion about browser cookies.
+    Stateless protocol. Shopping cart. Preferences. Session.
+    Controversial. EU-Cookie-Law (announce/opt-out).`
+  },
+
+  {
+    v: '_d0G6FZ_kR4',
+    t: 'EXTRA BITS - Follow the Cookie Trail',
+    d: '23rd May 2013',
+    n: `How many cookies - 3rd party.`
+  },
+
+  {
+    v: 'C3dU5u4xXaY',
+    t: 'Musical Floppy Drives',
+    d: '24th May 2013',
+    n: `Stepper motors (direction, step, signals) -
+    Arduino / Computer (Java)`
+  },
+
+  {
+    v: 'CpXaH77B8xg',
+    t: 'The Hair Algorithm',
+    d: '28th May 2013',
+    n: `Wet Hair, Apply Shampoo, Rinse, Repeat...
+    Train from Nottingham to London (time & cost)`
+  },
+  
+];
