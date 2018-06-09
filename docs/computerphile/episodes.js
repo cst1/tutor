59546d0109976dec50341fdbc6ec7bede98f86fa
Simon Hunt <sdhunt@gmail.com>
2018-06-09 13:50:40 -0700

    Added Computerphile episodes.

--- docs/computerphile/episodes.js
+++ docs/computerphile/episodes.js
@@ -43,8 +43,56 @@ computerphile = [
     v: 'CpXaH77B8xg',
     t: 'The Hair Algorithm',
     d: '28th May 2013',
-    n: `Wet Hair, Apply Shampoo, Rinse, Repeat...
+    n: `Wet Hair, Apply Shampoo, Wash, Rinse, Repeat...
     Train from Nottingham to London (time & cost)`
   },
-  
+
+  {
+    v: 'p7nGcY73epw',
+    t: 'The Art of Abstraction',
+    d: '31st May 2013',
+    n: `Widgets, abstracting ideas, transistors/1s and 0s ->,
+    Logic Gates -> instructions(+,-,*) ->
+    (human readable) script -> draw widget.
+    Layers of abstraction allow us to talk about stuff
+    at appropriate level. iPhone:black-box`
+  },
+
+  {
+    v: '7ZPW8FwgHoM',
+    t: 'Email',
+    d: '5th June 2013',
+    n: `how email messages work. header/body. parsing. ASCII.
+    abstraction/extension. MIME. 2nd level of encoding.
+    UUencode.`
+  },
+
+  {
+    v: 'sx2HjuZBuo0',
+    t: 'Raspbery Pi Hardware',
+    d: '7th June 2013',
+    n: `$25 Linux PC. ARM CPU 512MB ram, 750MHz,
+    GPIO (general purpose I/O) - 0.0->3.3v, 4LEDs/2btns example.
+    LED matrix, How its built, cases.
+    Aimed at programming and education, more robust.`
+  },
+
+  {
+    v: 'uNuFVq5QeRk',
+    t: 'Near to the Metal',
+    d: '12th June 2013',
+    n: `Emeritus Prof. David Brailsford; early computer science;
+    aware of the HW; science/art->linguistics;
+    Fortran,Java,C - syntax; changed over the years
+    (became commodities);`
+  },
+
+  {
+    v: 'do6xydtcVPk',
+    t: 'BBC B Microcomputer',
+    d: '14th June 2013',
+    n: `Home computer, Dec 1982, Acorn- series of TV progs.
+    under the hood, 2MHz 32K mem, OS in ROM (no disk); programming
+    BASIC, plot triangles`
+  },
 ];
